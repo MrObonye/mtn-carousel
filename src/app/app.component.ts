874545375641fb4241a153d4d8e073b06748b57c
L. Theodore Obonye <ltobonye@gmail.com>
2021-03-23 23:21:03 +0200

    refactor: response to 1920 res

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,18 +35,22 @@ export class AppComponent implements OnInit {
       1080: {
         slidesPerView: 5,
         spaceBetween: 16,
-        centeredSlides: true
+        centeredSlides: true,
+        loop: true
       },
       1366: {
         slidesPerView: 5,
         spaceBetween: 24,
         slidesOffsetBefore: 30,
-        centeredSlides: true
+        centeredSlides: true,
+        loop: true
       },
       1920: {
         slidesPerView: 5,
         spaceBetween: 24,
         centeredSlides: true,
+        slidesOffsetBefore: 34,
+        loop: true
       }
     }
   };
